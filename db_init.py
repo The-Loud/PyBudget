47f1c8f28668de3d3f261b33e14689d2937113d3
The-Loud <motorific@gmail.com>
2020-02-23 11:56:21 -0700

    refactored tables into classes

--- db_init.py
+++ db_init.py
@@ -1,5 +1,5 @@
 from connection import DB_ENG
-from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, declarative_base
 from sqlalchemy.dialects.mysql import \
         BIGINT, BINARY, BIT, BLOB, BOOLEAN, CHAR, DATE, \
         DATETIME, DECIMAL, DECIMAL, DOUBLE, ENUM, FLOAT, INTEGER, \
@@ -9,18 +9,46 @@ from sqlalchemy.dialects.mysql import \
 
 
 meta = MetaData()
+Base = declarative_base
 
 # Transaction table
-transactions = Table('transactions', meta,
-                     Column('id', Integer, primary_key=True, autoincrement=True, nullable=False),
-                     Column('created_at', TIMESTAMP, nullable=False),
-                     Column('updated_at', TIMESTAMP),
-                     Column('deleted_at', TIMESTAMP),
-                     Column('reconciled', TINYINT),
-                     Column('account_id', INTEGER, ForeignKey('accounts.id'), nullable=False),
-                     Column('description', VARCHAR(length=1024)),
-                     Column('amount', DECIMAL(precision=22, scale=2)))
+# Refactor these to have classes instead.
 
+class Transactions(Base):
+    __tablename__ = 'transactions'
+
+    p_id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False)
+    created_at = Column('created_at', TIMESTAMP, nullable=False)
+    updated_at = Column('updated_at', TIMESTAMP)
+    deleted_at = Column('deleted_at', TIMESTAMP)
+    reconciled = Column('reconciled', TINYINT)
+    account_id = Column('account_id', INTEGER, ForeignKey('accounts.id'), nullable=False)
+    description = Column('description', VARCHAR(length=1024))
+    amount = Column('amount', DECIMAL(precision=22, scale=2))
+
+    def __repr__(self):
+        return f"{self.p_id}, {created_at}, {updated_at},\
+                 {deleted_at}, {reconciled}, {account_id}, {description}, {amount}"
+
+
+class Accounts(Base):
+    __tablename__ = 'accounts'
+
+    p_id = Column('id', Integer, primary_key=True, autoincrement=True, nullable=False),
+    created_at = Column('created_at', TIMESTAMP, nullable=False),
+    updated_at = Column('updated_at', TIMESTAMP),
+    deleted_at = Column('deleted_at', TIMESTAMP),
+    account_id = Column('account_id', INTEGER(unsigned=True)),
+    description = Column('description', VARCHAR(length=1024)),
+    name = Column('name', VARCHAR(length=250), nullable=False))
+
+
+    def __repr__(self):
+        return f"{self.p_id}, {created_at}, {updated_at},\
+                 {deleted_at}, {account_id}, {description}, {name}"
+
+
+'''
 accounts = Table('accounts', meta,
                  Column('id', Integer, primary_key=True, autoincrement=True, nullable=False),
                  Column('created_at', TIMESTAMP, nullable=False),
@@ -29,5 +57,5 @@ accounts = Table('accounts', meta,
                  Column('account_id', INTEGER(unsigned=True)),
                  Column('description', VARCHAR(length=1024)),
                  Column('name', VARCHAR(length=250), nullable=False))
-
+'''
 meta.create_all(DB_ENG['sql'])
